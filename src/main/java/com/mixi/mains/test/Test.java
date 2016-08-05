7e8fa1848ffec0bdb251f3602ddffed378b586a9
tt5520 <898899528@qq.com>
2016-08-05 11:14:29 +0800

    新增注解方式操作数据库

---
+++ src/main/java/com/mixi/mains/test/Test.java
@@ -0,0 +1,11 @@
+package com.mixi.mains.test;
+
+
+public class Test {
+  public static void main(String[] args) {
+    // 生成1-100的随机数
+    long round = Math.round(Math.random() * (100 - 1) + 1);
+    System.out.println(round);
+  }
+}
+// 已更换固态硬盘
